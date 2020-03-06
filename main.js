8812ba30f7b357539630c65dffae3c239265ed9b
hara Prasad <haraprasad45@gmail.com>
2020-03-06 19:04:43 +0530

    minor changes

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
+  const buttons = document.querySelectorAll(".action-button");
+  const images = document.querySelectorAll(".image");
 
-const buttons = document.querySelectorAll(".action-button");
-const images = document.querySelectorAll(".image");
+  // checkfor media query
 
-// checkfor media query
+  //set innitial state of the components
+  const init = () => {
+    buttons[0].classList.add("focused");
+    buttons[0].classList.remove("button-hover");
+    images[0].style.display = "block";
+  };
 
-//set innitial state of the components
-const init = () => {
-  buttons[0].classList.add("focused");
-  buttons[0].classList.remove("button-hover");
-  images[0].style.display = "block";
-};
+  init();
 
-init();
+  function handleclick(e, index) {
+    //hide every image
+    images.forEach(element => {
+      element.style.display = "none";
+    });
+    //set to default class in every button
+    buttons.forEach(element => {
+      element.classList.replace("focused", "button-hover");
+    });
+    //add 'focused' class to clicked button
+    e.target.classList.replace("button-hover", "focused");
+    //display image on button click
+    images[index].style.display ='block';
+  }
 
-function handleclick(e, index) {
-  //hide every image
-  images.forEach(element => {
-    element.style.display = "none";
+  //click event handleer to the buttons
+  buttons.forEach((element, index) => {
+    element.addEventListener("click", function() {
+      handleclick(event, index);
+    });
   });
-  //set to default class in every button
-  buttons.forEach(element => {
-    element.classList.replace("focused", "button-hover");
-  });
-  //add 'focused' class to clicked button
-  e.target.classList.replace("button-hover", "focused");
-  //display image on button click
-  images[index].style.display ='block';
-}
-
-//click event handleer to the buttons
-buttons.forEach((element, index) => {
-  element.addEventListener("click", function() {
-    handleclick(event, index);
-  });
-});
